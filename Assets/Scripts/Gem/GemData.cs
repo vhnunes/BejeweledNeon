460f225f88fa724d13ab73a2e4fec4b11a6d75a2
Victor Nunes <vhndev@outlook.com>
2021-03-14 13:18:25 -0300

    Gem: Data Scriptable and sample

---
+++ Assets/Scripts/Gem/GemData.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace BJW
+{
+    [CreateAssetMenu(fileName = "Gem Data", menuName = "BJW/Gem Data", order = 0)]
+    public class GemData : ScriptableObject
+    {
+        #region Variables
+
+        [SerializeField] private GemType _gemType;
+        [SerializeField] private Sprite _gemSprite = null;
+        [SerializeField] private Color _gemColor = new Color();
+        
+        #region Properties
+
+        public GemType gemType => _gemType;
+        public Sprite gemSprite => _gemSprite;
+        public Color gemColor => _gemColor;
+
+        #endregion
+
+        #endregion
+
+    }
+}
