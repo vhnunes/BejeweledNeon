67a9b5c25f62939e4b87882ea0930815f31c44f4
Victor Nunes <vhndev@outlook.com>
2021-03-14 13:21:59 -0300

    Board: Manager monobehaviour to instance board and views.

---
+++ Assets/Scripts/Board/BoardManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using BJW;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    [SerializeField] private GemData[] _gemDatasToUseInGame = null; //TODO: Scriptable of gems game collection to use.
+    
+    private Board _board = null;
+
+    private void Start()
+    {
+        _board = new Board(8, 8, _gemDatasToUseInGame);
+    }
+}
