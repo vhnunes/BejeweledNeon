80a15900a97379852d59fb0374adbf30646b560a
Victor Nunes <vhndev@outlook.com>
2021-03-14 13:20:25 -0300

    Gem: Base Class w/ View Class and simple gem view creation using data

---
+++ Assets/Scripts/Gem/Gem.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+namespace BJW
+{
+    public class Gem
+    {
+        #region Variables
+
+        private GemType _gemType;
+        private Vector2 _boardPosition = new Vector2();
+
+        #endregion
+
+        #region Components
+
+        [SerializeField] private GemData _gemData = null;
+        
+        private GemView _gemView = null;
+
+        #endregion
+
+        #region Methods
+        
+        // Constructor
+        public Gem(GemData gemData)
+        {
+            _gemData = gemData;
+            _gemType = gemData.gemType;
+            
+            CreateView();
+        }
+
+        public void SetPosition(Vector2 newPosition)
+        {
+            _boardPosition = newPosition;
+            _gemView.UpdatePosition(newPosition);
+        }
+        
+        private void CreateView()
+        {
+            _gemView = new GameObject().AddComponent<GemView>();
+            _gemView.SetGemData(_gemData);
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/Scripts/Gem/GemView.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using BJW;
+using UnityEngine;
+
+public class GemView : MonoBehaviour
+{
+    #region Components
+
+    [SerializeField] private GemData _gemData = null;
+    private SpriteRenderer _renderer = null;
+
+    #endregion
+
+    #region MonoBehaviour
+    
+    private void Start()
+    {
+        InitializeRenderer();
+    }
+
+    #endregion
+    
+    #region Methods
+
+    private void InitializeRenderer()
+    {
+        _renderer = this.gameObject.AddComponent<SpriteRenderer>();
+        _renderer.sprite = _gemData.gemSprite;
+        _renderer.color = _gemData.gemColor;
+    }
+    public void UpdatePosition(Vector2 newPosition)
+    {
+        this.transform.position = newPosition;
+    }
+
+    public void SetGemData(GemData newGemData)
+    {
+        _gemData = newGemData;
+    }
+
+    #endregion
+}
